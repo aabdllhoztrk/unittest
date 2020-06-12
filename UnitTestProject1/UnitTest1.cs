4535caf2e80b417a9199b321e929239428d013ea
Abdullah Ozturk <ozturkaabdullah@hotmail.com>
2020-06-12 20:16:06 +0300

    ilk commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver(@"c:\");
+            driver.Navigate().GoToUrl("https://www.globalsqa.com/samplepagetest/");
+            driver.Manage().Window.Maximize();
+
+            driver.FindElement(By.Id("g2599-name")).SendKeys("Hello");
+            driver.FindElement(By.Id("g2599-email")).SendKeys("ozturk@gmail.com");
+
+            driver.FindElement(By.ClassName("checkbox-multiple")).Click();
+
+            driver.FindElement(By.Name("g2599-comment")).SendKeys("Test Comments");
+
+            
+
+            driver.Close();
+        }
+    }
+}
